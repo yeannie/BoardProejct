40903959f0494e80061d613f7539f6b7cc674bf3
pc <pc@yeannie-gim>
2022-01-18 12:09:56 +0900

    ~service

---
+++ src/main/java/board/board/BoardService/BoardService.java
@@ -0,0 +1,19 @@
+package board.board.BoardService;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import board.board.dto.BoardDto;
+
+@Service
+public class BoardService {
+
+	/*
+	 * public List<BoardDto> selectBoardList() {
+	 *  //Controller에서 자동으로 만들어진 매서드
+	 * return null; }
+	 */
+	
+	
+}
